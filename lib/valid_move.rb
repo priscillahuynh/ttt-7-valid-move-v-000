2dcdaa62fae390471492398cf04099e732cbdf9a
Priscilla H <priscillamhuynh@gmail.com>
2018-06-06 11:53:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if between? == true && position_taken? == true
+  if board[index]== .between?(1,9) && position_taken? == true
     Move = true
   else
     Move = false
@@ -15,10 +15,3 @@ def position_taken?(board,index)
 end# code your #position_taken? method here!
 end
 
-def between?(int)
-  if int.clamp(1, 9)
-    valid = true
-  else
-    valid = false
-  end
-end
